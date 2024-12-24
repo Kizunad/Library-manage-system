2876fae0cac19756523d42a374bce743adc8ff5f
kizuna <lizitainz@gmail.com>
2024-12-24 23:46:37 +1300

    finished borrowingrec.hpp

--- backend/src/controllers/book_controller.cpp
+++ backend/src/controllers/book_controller.cpp
@@ -1,6 +1,7 @@
 // src/controllers/book_controller.cpp
 
 #include "controllers/book_controller.hpp"
+#include "models/book.hpp"
 #include <exception>
 #include <string>
 
@@ -39,6 +40,41 @@ nlohmann::json BookController::handleGetAllBooks(
 
     } catch (const std::exception& e) {
         //TODO Logging
+        return {
+            {"success", false},
+            {"error", e.what()}
+        };
+    }
+}
+
+nlohmann::json BookController::handleAddBook(const nlohmann::json& book_data) {
+    try {
+        auto book = bookService_.addBook(
+            book_data["isbn"],
+            book_data["title"],
+            book_data["author"],
+            book_data["publisher"],
+            book_data["publishDate"],
+            book_data["category"],
+            book_data["totalCopies"]
+        );
+
+        if (book) {
+            return {
+                {"success", true},
+                {"book", {
+                    {"id", book->getId()},
+                    {"isbn", book->getIsbn()},
+                    {"title", book->getTitle()}
+                }}
+            };
+        }
+
+        return {
+            {"success",false},
+            {"error","Failed to add Book."}
+        };
+    } catch (const std::exception& e) {
         return {
             {"success", false},
             {"error", e.what()}
---
+++ backend/src/models/borrowing_record.cpp
@@ -0,0 +1,4 @@
+// src/models/borrowing_record.cpp
+
+#include "models/borrowing_record.hpp"
+#include "utils/database_pool.hpp"
